8c2f742a4cd1bffc10a112ea65f47c5c49cc26cb
Paz <mariapazgar@gmail.com>
2022-10-13 14:30:19 +0100

    Added Github OAuth

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,12 @@
 const { homeHtml } = require("../templates/home");
+const dotenv = require("dotenv");
+dotenv.config();
 
 function get(request, response) {
-  response.send(homeHtml());
+  const client_id = process.env.client_id;
+  const login_url = `https://github.com/login/oauth/authorize?client_id=${client_id}`;
+
+  response.send(homeHtml(login_url));
 }
 
 module.exports = { get };
